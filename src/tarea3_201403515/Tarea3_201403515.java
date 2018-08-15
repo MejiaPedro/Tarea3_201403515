083ea8eca6b03025a4aaf8ebbd4f0a47189f87da
ptmej <ptmej@DESKTOP-7QPMSLC>
2018-08-15 10:24:14 -0600

    Tarea3 inicializada

---
+++ src/tarea3_201403515/Tarea3_201403515.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea3_201403515;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ptmej
+ */
+public class Tarea3_201403515 {
+
+  
+    public static void main(String[] args) {
+       int opcion;
+        opcion = 0;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("1.Usuarios");
+        System.out.println("2.Contador de Digitos Repetidos");
+        System.out.println("3.Tres Numero de Mayor a Menor");
+        System.out.println("4.Calcular Promedio");
+        System.out.println("5.Salir");
+        
+        opcion = entrada.nextInt();
+        
+    }
+    
+}
